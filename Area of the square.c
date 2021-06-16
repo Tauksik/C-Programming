7081c8ed11da6446b98b0e9a37e33d07d4e8c90a
Tauksik <84122841+Tauksik@users.noreply.github.com>
2021-06-17 01:15:05 +0530

    Create Area of the square.c

---
+++ Area of the square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+float s,area;
+printf("Enter the side of the square : ");
+scanf("%f",&s);
+area=s*s;
+printf("The area of the square is =%.2f",area);
+return 0;
+}
