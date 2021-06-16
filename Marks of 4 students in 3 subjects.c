1392ed622c9c755acf28ae8be24ed5f281932e5b
Tauksik <84122841+Tauksik@users.noreply.github.com>
2021-06-17 01:03:19 +0530

    Create Marks of 4 students in 3 subjects.c

---
+++ Marks of 4 students in 3 subjects.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i,j,r,c;
+printf("\nInput the number of students and number of subjects: "); scanf("%d %d",&r,&c);
+int a[100][100],s[100]; printf("Input Data:\n"); for(i=0;i<r;i++)
+{ printf("\n");
+for(j=0;j<c;j++) {
+printf("Student [%d] marks in subject [%d] : ",i+1,j+1);
+scanf("%d",&a[i][j]); }
+} for(j=0;j<c;j++)
+{ s[j]=a[0][j]; for(i=0;i<r;i++)
+{
+if (s[j] < a[i][j]) s[j] = a[i][j];
+} }
+for(i=0;i<c;i++) {
+printf("\n");
+printf("Highest in Subject[%d] is %d",i+1,s[i]); }
+printf("\n"); return 0;
+}
